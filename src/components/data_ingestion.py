ed97595abe3998dbdd35bb728e1acc5ee631aee2
Rakhi Kumari <rakhiwadhwani47@gmail.com>
2026-04-17 04:01:50 +0500

    Data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import numpy as np
 from src.exception import customException
 from src.logger import logging
 import pandas as pd
@@ -35,7 +36,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -48,3 +49,8 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+
+    data_transformation= DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
